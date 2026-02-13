b7e39af8dcd09d0f1d740cf62b1ad228f6ab1877
Nanasina <hasinabefanambinana09@gmail.com>
2026-02-13 22:16:08 +0100

    ajout d'un modal pour le btn ajouter un film ou série

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,12 @@
 import { Plus, Clapperboard } from "lucide-react";
+// import { motion, Typewriter } from "motion/react"
 
 function List() {
 
   return (
     <>
       <div className="flex gap-8 m-8">
-        <h1 className="font-mono text-3xl font-bolds flex"> <Clapperboard className="m-2 animate-bounce"/> FILMS & SERIES</h1>
+        <h1 className="font-mono text-3xl font-bolds flex"> <Clapperboard className="m-2 animate-bounce"/>FILMS & SERIES</h1>
         <div>
           <label className="input">
             <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -29,7 +30,7 @@ function List() {
      <button className="btn btn-soft">Vu</button>
 
      <div className="ml-15">
-        <button className="btn btn-soft btn-secondary "> <Plus />Ajouter un film/série</button>
+        <button className="btn btn-soft btn-secondary " onClick={()=>document.getElementById('my_modal_3').showModal()}> <Plus />Ajouter un film/série</button>
      </div>
      
     </div>
@@ -41,7 +42,8 @@ function List() {
     <thead>
       <tr>
         <th>Titre</th>
-        <th>Type</th>
+        <th>Film ou Série</th>
+        <th>Catégorie</th>
         <th>Status</th>
         <th>Nombre de vu</th>
         <th>Actions</th>
@@ -56,6 +58,44 @@ function List() {
     </tbody>
   </table>
 </div>
+    </div>
+
+    <div>
+      <dialog id="my_modal_3" className="modal">
+  <div className="modal-box">
+    <form method="dialog">
+      {/* if there is a button in form, it will close the modal */}
+      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+    </form>
+    <h3 className="font-bold text-lg text-center">AJOUTER UN FILM OU UNE SERIE</h3>
+   <div className="m-8">
+    <fieldset className="fieldset">
+      <legend className="fieldset-legend">Titre</legend>
+      <input type="text" className="input w-95" placeholder="Titre du film" />
+  </fieldset>
+
+   <fieldset className="fieldset">
+      <legend className="fieldset-legend">Film ou série</legend>
+      <select defaultValue="Film ou série" className="select w-95">
+        <option disabled={true} >Film ou série</option>
+        <option>Film</option>
+        <option>Série</option>
+        
+     </select>
+  </fieldset>
+
+   <fieldset className="fieldset">
+      <legend className="fieldset-legend">Catégorie</legend>
+      <input type="text" className="input w-95" placeholder="exemple: K-drama" />
+  </fieldset>
+   </div>
+
+   <div className="flex justify-center items-center">
+    <button className="btn btn-soft btn-secondary btn-wide ">AJOUTER</button>
+   </div>
+    
+  </div>
+</dialog>
     </div>
     </>
   )
